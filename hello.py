ed298405b7a104044f32eed035bc771d9db9b152
vengiezsny <c20366171@mytudublin.ie>
2025-09-26 11:59:42 +0100

    Updated /about route with Flask and Python links

--- hello.py
+++ hello.py
@@ -8,9 +8,14 @@ app = Flask(__name__)
 def say_hello():
 	return '<p>Welcome, I am a Flask app!</p><a href="/about">About Page</a><a href="/contact">Contact Page</a>'
 	
-@app.route('/about')
-def say_hello():
-	return '<p>Hello, I am the about page!</p>'
+@app.route("/about")
+def about():
+    return '''
+    <h1>About Page</h1>
+    <p>This app uses Flask and Python.</p>
+    <a href="https://flask.palletsprojects.com/">Flask Website</a><br>
+    <a href="https://www.python.org/">Python Website</a>
+    '''
     
 @app.route('/contact')
 def say_hello():
