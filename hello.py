ec7ba11f253ca895c3a08934ac8a41a3f5f1d475
JohnH <c22455366@mytudublin.ie>
2025-09-26 11:18:25 +0100

    Added Contact Page

--- hello.py
+++ hello.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>Hello, World, I am a Flask app!</p> <a href="/about">About Page</a>'
+	return '<p>Hello, World, I am a Flask app!</p><a href="/about">About Page</a><a href="/contact">Contact Page</a>'
 	
 @app.route('/about')
 def say_hello():
 	return '<p>Hello, I am the about page!</p>'
+    
+@app.route('/contact')
+def say_hello():
+	return '<p>Email: c22455366@mytudublin.ie</p>'
