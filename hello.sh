4a7e93279e8196cded40e12451eeef7f908f14a8
cel9029 <C22455366@mytudublin.ie>
2025-11-07 12:50:25 +0000

    Kubernetes branch update

--- hello.sh
+++ hello.sh
@@ -7,24 +7,59 @@ python -m venv .my_venv
 source .my_venv/bin/activate
 pip install flask
 pip install redis
-cat << script < hello.py
+cat << script > hello.py
 from flask import Flask
+import redis
+import time
+import json
 
-
+r = redis.Redis(host='the-redis-server', port=6379, decode_responses=True)
 app = Flask(__name__)
-
 
 @app.route('/')
 def say_hello():
-	return '<p>Welcome, I am a Flask app!</p> <a href="/about">About Page</a><a href="/contact">Contact Page</a>'
-	
-@app.route('/about')
+    data = r.get('/')
+    if data is not None:
+        data = json.loads(data)
+        if time.time() - data['time'] <= 600:
+            return data['html']
+
+    data = '<p>Welcome, I am a Flask app!</p><a href="/about">About Page</a><a href="/contact">Contact Page</a>'
+    r.set('/', json.dumps({
+        'html': data,
+        'time': time.time()
+    }))
+    return data
+
+@app.route("/about")
 def about():
-	return '<p>Hello, I am the about page!</p>'
+    data = r.get('about')
+    if data is not None:
+        data = json.loads(data)
+        if time.time() - data['time'] <= 600:
+            return data['html']
+
+    data = '<h1>About Page</h1><p>This app uses Flask and Python.</p><br><a href="https://flask.palletsprojects.com/">Flask Website</a><br><a href="https://www.python.org/">Python Website</a>'
+    r.set('about', json.dumps({
+        'html': data,
+        'time': time.time()
+    }))
+    return data
 
 @app.route('/contact')
 def contact():
-	return '<p>Email: C22455366@mytudublin.ie</p>'
+    data = r.get('contact')
+    if data is not None:
+        data = json.loads(data)
+        if time.time() - data['time'] <= 600:
+            return data['html']
+
+    data = '<p>Email: C22455366@mytudublin.ie</p>'
+    r.set('contact', json.dumps({
+        'html': data,
+        'time': time.time()
+    }))
+    return data
 script
 
 
